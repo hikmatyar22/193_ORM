5ee7395f5316714c1cae94a737b80607ea67eb83
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-10-28 15:43:05 +0700

    menggubah timestamp menjadi true agar muncul

--- models/komik.js
+++ models/komik.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
   
     }, {
       tableName: "komik",
-      timestamps: false,
+      timestamps: true,
       freezeTableName: true
     });
     return Komik;
