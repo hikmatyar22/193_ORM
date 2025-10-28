ce4b778c7ba6e36cf886f67c1d1842d4cf5bbd83
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-10-28 15:34:07 +0700

    membuat komik.js dan membuat isi tabel dari komik yang pertama yakni id komik

---
+++ models/komik.js
@@ -0,0 +1,11 @@
+module.exports = (sequelize, DataTypes) => {
+    const Komik = sequelize.define("Komik", {
+        
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+
+    });
+  }
